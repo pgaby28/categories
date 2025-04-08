bb5cd92c9ccfaa919712398e672b527876454f5f
Gabriela Pérez <gabriela.perez@behit.cat>
2025-04-09 00:24:57 +0200

    initial vue setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import './assets/main.css'
+import "@/assets/css/main.scss";
+import "@mdi/font/css/materialdesignicons.css";
 
-import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/styles';
 
-import App from './App.vue'
+import App from './App.vue';
 
 const vuetify = createVuetify({
   components,
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    vuetify({ autoImport: true }),
   ],
   resolve: {
     alias: {
